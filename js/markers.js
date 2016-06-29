b26a4eeaac40f23888cd414997c56d99ed3b4732
manniswithaplannis <nicolbolas123@gmail.com>
2016-06-29 16:17:51 -0400

    Pauix's stuff

--- js/markers.js
+++ js/markers.js
@@ -9,38 +9,63 @@ var SLcapitol = L.icon({iconUrl: 'img/marker/SLcapitol.png',iconSize: [32, 74]})
 var SLfort = L.icon({iconUrl: 'img/marker/SLfort.png',iconSize: [32, 74]})
 var SLtown = L.icon({iconUrl: 'img/marker/SLtown.png',iconSize: [32, 74]})
 var SLpalace = L.icon({iconUrl: 'img/marker/SLpalace.png',iconSize: [32, 74]})
+
 var CL = L.icon({iconUrl: 'img/marker/CL.png',iconSize: [32, 74]})
 var CLcity = L.icon({iconUrl: 'img/marker/CLcity.png',iconSize: [32, 74]})
 var CLfort = L.icon({iconUrl: 'img/marker/CLfort.png',iconSize: [32, 74]})
 var CLtown = L.icon({iconUrl: 'img/marker/CLtown.png',iconSize: [32, 74]})
+
 var Dorne = L.icon({iconUrl: 'img/marker/Dorne.png',iconSize: [32, 74]})
 var DorneCapitol = L.icon({iconUrl: 'img/marker/DorneCapitol.png',iconSize: [32, 74]})
+
 var II = L.icon({iconUrl: 'img/marker/II.png',iconSize: [32, 74]})
 var IItown = L.icon({iconUrl: 'img/marker/IItown.png',iconSize: [32, 74]})
 var IIcapitol = L.icon({iconUrl: 'img/marker/IIcapitol.png',iconSize: [32, 74]})
+
 var North = L.icon({iconUrl: 'img/marker/North.png',iconSize: [32, 74]})
 var NorthCity = L.icon({iconUrl: 'img/marker/NorthCity.png',iconSize: [32, 74]})
 var NorthTown = L.icon({iconUrl: 'img/marker/NorthTown.png',iconSize: [32, 74]})
 var NorthCapitol = L.icon({iconUrl: 'img/marker/NorthCapitol.png',iconSize: [32, 74]})
 var NorthFort = L.icon({iconUrl: 'img/marker/NorthFort.png',iconSize: [32, 74]})
-var NotWesteros = L.icon({iconUrl: 'img/marker/NotWesteros.png',iconSize: [32, 74]})
+
 var Reach = L.icon({iconUrl: 'img/marker/Reach.png',iconSize: [32, 74]})
 var ReachFort = L.icon({iconUrl: 'img/marker/ReachFort.png',iconSize: [32, 74]})
 var ReachTown = L.icon({iconUrl: 'img/marker/ReachTown.png',iconSize: [32, 74]})
 var ReachCity = L.icon({iconUrl: 'img/marker/ReachCity.png',iconSize: [32, 74]})
 var ReachCapitol = L.icon({iconUrl: 'img/marker/ReachCapitol.png',iconSize: [32, 74]})
+
 var RL = L.icon({iconUrl: 'img/marker/RL.png',iconSize: [32, 74]})
+
 var TrueNorth = L.icon({iconUrl: 'img/marker/TrueNorth.png',iconSize: [32, 74]})
+
 var Vale = L.icon({iconUrl: 'img/marker/Vale.png',iconSize: [32, 74]})
 var Valecapitol = L.icon({iconUrl: 'img/marker/Valecapitol.png',iconSize: [32, 74]})
 var Valecity = L.icon({iconUrl: 'img/marker/Valecity.png',iconSize: [32, 74]})
 var Valetown = L.icon({iconUrl: 'img/marker/Valetown.png',iconSize: [32, 74]})
 var Valefort = L.icon({iconUrl: 'img/marker/Valefort.png',iconSize: [32, 74]})
+
 var Wall = L.icon({iconUrl: 'img/marker/Wall.png',iconSize: [32, 74]})
 var West = L.icon({iconUrl: 'img/marker/West.png',iconSize: [32, 74]})
 var WestCapitol = L.icon({iconUrl: 'img/marker/WestCapitol.png',iconSize: [32, 74]})
 var WestCity = L.icon({iconUrl: 'img/marker/WestCity.png',iconSize: [32, 74]})
 var WestTown = L.icon({iconUrl: 'img/marker/WestTown.png',iconSize: [32, 74]})
+
+var Essos = L.icon({iconUrl: 'img/marker/NotWesteros.png',iconSize: [32, 74]})
+var EssosFort = L.icon({iconUrl: 'img/marker/EssosFort.png',iconSize: [32, 74]})
+var EssosRuin = L.icon({iconUrl: 'img/marker/EssosRuin.png',iconSize: [32, 74]})
+var EssosTown = L.icon({iconUrl: 'img/marker/EssosTown.png',iconSize: [32, 74]})
+var EssosCity = L.icon({iconUrl: 'img/marker/EssosCity.png',iconSize: [32, 74]})
+var EssosBigCity = L.icon({iconUrl: 'img/marker/EssosBigCity.png',iconSize: [32, 74]})
+
+var ValyriaRuin = L.icon({iconUrl: 'img/marker/ValyriaRuin.png',iconSize: [32, 74]})
+var ValyriaCity = L.icon({iconUrl: 'img/marker/ValyriaCity.png',iconSize: [32, 74]})
+
+var SothoryosRuin = L.icon({iconUrl: 'img/marker/SothoryosRuin.png',iconSize: [32, 74]})
+
+var Rhoynish = L.icon({iconUrl: 'img/marker/Rhoynish.png',iconSize: [32, 74]})
+
+var SICity = L.icon({iconUrl: 'img/marker/SICity.png',iconSize: [32, 74]})
+var SITown = L.icon({iconUrl: 'img/marker/SITown.png',iconSize: [32, 74]})
 
 // Define the markers
 var markers = [
@@ -244,6 +269,90 @@ var markers = [
 [-5.9657536710655235,-132.5390625,West,"The Bower"],
 [-6.664607562172573,-138.8671875,West,"Cornfield"],
 [2.6357885741666065,-143.876953125,WestCity,"Lannisport"],
+
+// Free Cities
+[41.3108238809182,-44.12109374999999,EssosBigCity,"Braavos"],
+[-2.7235830833483856,-43.681640625,EssosBigCity,"Pentos"],
+[14.093957177836224,-15.205078125,EssosBigCity,"Norvos"],
+[-2.0210651187669897,11.337890625,EssosBigCity,"Qohor"],
+[-29.91685223307016,-33.57421875,EssosBigCity,"Myr"],
+[-50.95842672335992,-42.01171875,EssosBigCity,"Lys"],
+[-33.797408767572485,-54.404296875,EssosBigCity,"Tyrosh"],
+[36.24427318493909,-21.97265625,EssosBigCity,"Lorath"],
+[-51.72702815704775,5.625,EssosBigCity,"Volantis"],
+
+//Old Valyria
+[-52.908902047770255,46.142578125,ValyriaRuin,"Elyria"],
+[-61.8146638946839,60.99609375,ValyriaRuin,"Velos"],
+[-57.23150299147891,59.32617187499999,ValyriaRuin,"Ghozai"],
+[-67.67608458198097,35.68359375,ValyriaRuin,"Tyria"],
+[-68.91100484562018,21.533203125,ValyriaRuin,"Oros"],
+[-71.01695975726373,31.904296874999996,ValyriaRuin,"Valyria"],
+
+//// Rest of Essos
+[1.3182430568620136,-28.125,EssosRuin,"Ghoyan Drohe"],
+[-7.536764322084078,-12.480468749999998,EssosRuin,"Ny Sar"],
+[60.71619779357716,144.22851495444772,EssosCity,"Port of Ibben"],
+[68.52823492039876,149.06249932944772,EssosTown,"Ib Nor"],
+[50.401515322782366,170.15624932944775,EssosTown,"Ib Saar"],
+[43.51668853502909,157.67578057944775,EssosRuin,"Ibbish"],
+[47.39834920035926,143.61328057944775,EssosTown,"New Ibbish"],
+[33.50475906922606,54.75585870444774,EssosRuin,"Sarys"],
+[38.548165423046584,49.13085870444774,EssosCity,"Morosh"],
+[31.87755764334002,39.814452454447746,EssosCity,"Saath"],
+[-0.7031073524364783,33.925780579447746,EssosRuin,"Essaria"],
+[-6.577303118123875,62.929686829447746,EssosRuin,"Sarnath"],
+[-2.3723687086440504,80.85937432944775,EssosRuin,"Kasath"],
+[-6.402648405963884,93.51562432944775,EssosRuin,"Sathar"],
+[10.228437266155943,86.48437432944775,EssosRuin,"Sallosh"],
+[3.7765593098768635,90.96679620444775,EssosRuin,"Gornath"],
+[-20.797201434306984,83.05663995444775,EssosRuin,"Hazdahn No"],
+[10.487811882056683,140.88867120444775,EssosTown,"Vaes Dothrak"],
+[-40.78054143186031,-64.423828125,EssosFort,"Bloodstone (T1 Fort and T1 Port)"],
+[-47.57652571374621,40.517577454447746,ValyriaCity,"Mantarys"],
+[-47.04018214480664,30.49804620444775,ValyriaRuin,"Ruins"],
+[-48.63290858589533,21.181639954447746,ValyriaRuin,"Ruins"],
+[-51.835777520452474,52.82226495444774,EssosCity,"Tolos"],
+[-66.687783861162,82.61718682944775,EssosRuin,"Old Ghis"],
+[-73.022591571473,86.57226495444775,EssosCity,"New Ghis"],
+[-57.32652122521709,78.57421807944775,EssosBigCity,"Astapor"],
+[-45.95114968669139,82.52929620444775,EssosBigCity,"Yunkai"],
+[-40.913512576127566,89.64843682944775,EssosBigCity,"Meereen"],
+[-66.6181218846659,166.99218682944775,EssosBigCity,"Qarth"],
+[-62.71446210149774,155.47851495444775,EssosRuin,"Vaes Qosar"],
+[-57.23150299147891,144.66796807944775,EssosRuin,"Vaes Tolorro"],
+[-61.81466389468391,138.60351495444775,EssosRuin,"Vaes Shirosi"],
+[-63.430860212678105,129.19921807944775,EssosRuin,"Vaes Orvik"],
+[-66.08936427047087,127.35351495444773,EssosTown,"Port Yhos"],
+[-64.92354174306496,147.91992120444775,EssosTown,"Qarkash"],
+[-41.64007838467892,127.44140557944775,EssosCity,"Kosrak"],
+[-30.902224705171427,160.83984307944775,EssosRuin,"Adakhakileki"],
+[-26.509904531413927,166.37695245444775,EssosRuin,"Vaes Jini (Yinishar)"],
+[-21.616579336740593,135.08788995444775,EssosRuin,"Vaes Efe"],
+[-28.381735043223095,129.19921807944775,EssosRuin,"Vaes Mejhah"],
+[-29.305561325527712,119.26757745444775,EssosRuin,"Qhardaq"],
+[-41.70572851523751,108.01757745444775,EssosCity,"Hesh"],
+[-49.667627822621924,115.57617120444775,EssosCity,"Lhazosh"],
+[-28.226970038918328,-7.207031920552255,EssosRuin,"The Sorrows"],
+[-40.380028402511826,-4.218750670552254,EssosTown,"Selhorys"],
+[-45.151053265563384,-4.218750670552254,EssosTown,"Valysar"],
+[-48.6909603909255,-2.0214850455522537,EssosTown,"Volon Therys"],
+// Summer Islands
+[-77.80477074199555,-46.757813170552254,SITown,"Tall Trees Town"],
+[-76.41397301513199,-56.601563170552254,SICity,"Lotus Port"],
+[-74.54332982677904,-65.39062567055225,SITown,"Last Lament"],
+
+
+// Sothoryos 
+[-80.8588748843299,74.619140625,SothoryosRuin,"Zamettar"],
+[-83.03688198552364,81.5625,SothoryosRuin,"Yeen"],
+[-82.48333497678894,62.75390625,SothoryosRuin,"Gogossos"],
+[-79.68718415450823,132.099609375,SothoryosRuin,"Gorosh"],
+
+// Moraq
+[-76.43460358351301,158.3789049088955,EssosTown,"Vahar"],
+[-73.52839948765174,161.3671861588955,EssosTown,"Faros"],
+[-80.61842419685506,179.1210924088955,EssosTown,"Port Moraq"],
 
 
 ]
